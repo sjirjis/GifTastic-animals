076cb3747d7b9570330d86117504df8971e8a184
School <School>
2017-02-25 08:25:46 -0800

    hw fix push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,63 +10,58 @@ var static = {
     ],
 };
 
-for(i = 0; i < static.sButtons.length; i++){
-	$('.staticButtons').append('<button class="sbtn" value=' + static.sButtons[i] + '>' + static.sButtons[i]);
+for (i = 0; i < static.sButtons.length; i++) {
+    $('.staticButtons').append('<button class="sbtn" value=' + static.sButtons[i] + '>' + static.sButtons[i]);
 };
 
 //create api call
-var giphyResults = function(animal){
+var giphyResults = function(animal) {
 
     var apiKey = 'api_key=dc6zaTOxFJmzC';
     var qURL = 'http://api.giphy.com/v1/gifs/search?q=' + animal + '&' + apiKey + '&limit=10';
 
     $.ajax({
-        url: qURL,
-        method: 'GET'
-    })
-    .done(function(response){
-//what to display in the call result   
+            url: qURL,
+            method: 'GET'
+        })
+        .done(function(response) {
+            //what to display in the call result   
 
-//first clear out the div contents...
-	$('.gifResults').html("");
+            //first clear out the div contents...
+            $('.gifResults').html("");
 
-//...now put the new stuff	
-	for (i = 0; i < response.data.length; i++)	
-	    $('.gifResults').append('<h4 class="rating">Rating: ' + response.data[i].rating + '</h4>'
-			+ '<img src="' + response.data[i].images.fixed_height_still.url 
-			+ '" data-animate="' + response.data[i].images.fixed_height.url 
-			+ '" data-still="' + response.data[i].images.fixed_height_still.url + '" /> <br/>')duck
-	    console.log(response);
-	});
+            //...now put the new stuff	
+            for (i = 0; i < response.data.length; i++)
+                $('.gifResults').append('<h4 class="rating">Rating: ' + response.data[i].rating + '</h4>' 
+                	+ '<img src="' + response.data[i].images.fixed_height_still.url 
+                	+ '" data-animate="' + response.data[i].images.fixed_height.url 
+                	+ '" data-still="' + response.data[i].images.fixed_height_still.url + '" /> <br/>')
+        });
 };
 
 //display api call results as required on button click
-$('body').on('click', '.sbtn', function(){
-	giphyResults(
-		$(this).val()
-	);
-	console.log(
-		$(this).val()
-	);
+$('body').on('click', '.sbtn', function() {
+	var $thisVal = $(this).val()
+    giphyResults($thisVal);
 });
 
 //append user button to static buttons
-$('.addAnimalbtn').on('click', function(){
-	var uVal = $('.uInput').val();
-	$('.staticButtons').append('<button class="sbtn" value=' + uVal + '>' + uVal);
+$('.addAnimalbtn').on('click', function() {
+    var uVal = $('.uInput').val();
+    $('.staticButtons').append('<button class="sbtn" value=' + uVal + '>' + uVal);
 });
 
 //create animation
-$('body').on('click', 'img', function(){
-	var $imgStill = $(this).attr('data-still'),
-	$imgAnimate = $(this).attr('data-animate')
-	$imgSrc = $(this).attr('src')
+$('body').on('click', 'img', function() {
+    var $imgStill = $(this).attr('data-still'),
+        $imgAnimate = $(this).attr('data-animate'),
+        $imgSrc = $(this).attr('src')
 
-	if (
-		$imgSrc === $imgStill
-	) {
-		$(this).attr('src', $imgAnimate);
-	} else {
-		$(this).attr('src', $imgStill);
-	}
+    if (
+        $imgSrc === $imgStill
+    ) {
+        $(this).attr('src', $imgAnimate);
+    } else {
+        $(this).attr('src', $imgStill);
+    }
 });
